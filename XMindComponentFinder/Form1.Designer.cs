edd87d17640acae5f84315f5f11cdad7ef615372
konstantin <ershov.k@outlook.com>
2016-11-23 15:47:46 +0100

    *

---
+++ XMindComponentFinder/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace XMindComponentFinder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(515, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.richTextBox2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(515, 350);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox2.Location = new System.Drawing.Point(3, 33);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(509, 314);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(509, 24);
+            this.panel1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(106, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(73, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Find all";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 374);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ XMindComponentFinder/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace XMindComponentFinder
+{
+    public partial class Form1 : Form
+    {
+        private string scProjPath;
+        XDocument document;
+        public Form1()
+        {
+            InitializeComponent();
+            openFileDialog1.Filter = "Xmind book|*.xmind";
+            openFileDialog1.FileOk += (sender, args) =>
+            {
+                scProjPath = openFileDialog1.FileName;
+                using (ZipArchive archive = ZipFile.Open(scProjPath, ZipArchiveMode.Read))
+                {
+                    ZipArchiveEntry entry = archive.GetEntry("content.xml");
+                    using (var reader = XmlReader.Create(entry.Open()))
+                    {
+                        document = XDocument.Load(reader);
+                    }
+                }
+            };
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (document == null)
+            {
+                MessageBox.Show("Load document firstly");
+                return;
+            }
+
+            var componentName = textBox1.Text.Trim();
+
+            if (componentName.Length == 0)
+            {
+                MessageBox.Show("Specify component for search");
+                return;
+            }
+
+            var nameSpace = document.Root.Name.NamespaceName;
+            var foundElements = document.Descendants(XName.Get("topic", nameSpace))
+                .Where(t => componentName.Equals(t.Element(XName.Get("title", nameSpace))?.Value, StringComparison.InvariantCultureIgnoreCase));
+
+            // remove first part (sheet name)
+            var paths = foundElements.Select(elem => Regex.Replace(GetPath(elem), "^\\/[^/]*\\/", ""));
+
+            paths = DemergeLayouts(paths).OrderBy(s => s);
+
+            richTextBox2.Text = string.Join(Environment.NewLine, paths);
+        }
+
+        static IEnumerable<string> DemergeLayouts(IEnumerable<string> paths)
+        {
+            foreach (var path in paths)
+            {
+                var parts = path.Split('/');
+                var multiLayoutIndex = parts.Select((p, i) => new { Index = i, Part = p })
+                    .SingleOrDefault(p => p.Part.Contains("&"))?.Index;
+                if (multiLayoutIndex == null)
+                    yield return path;
+                else
+                {
+                    var layouts = Regex.Split(parts[multiLayoutIndex.Value], " *& *");
+                    foreach (var layout in layouts)
+                    {
+                        parts[multiLayoutIndex.Value] = layout;
+                        yield return string.Join("/", parts);
+                    }
+                }
+            }
+        }
+
+        private static Dictionary<string, string> namingNullMap = new Dictionary<string, string>
+        {
+            {"directly within ", null},
+            {" components", null }
+        };
+
+        private static string GetPath(XElement element)
+        {
+            if (element == null)
+            {
+                throw new ArgumentNullException("element");
+            }
+
+            Func<XElement, string> relativeXPath = e =>
+            {
+                var title = e.Element(XName.Get("title", e.Name.NamespaceName))?.Value;
+
+
+                if (title == null)
+                    return null;
+
+                var foundKey = namingNullMap.Keys.FirstOrDefault(k => title.Contains(k));
+                if (foundKey != null)
+                {
+                    title = namingNullMap[foundKey];
+                }
+
+                return title?.Replace(Environment.NewLine, "").Insert(0, "/");
+            };
+
+            var ancestors = element.Ancestors().Select(relativeXPath);
+
+            return string.Concat(ancestors.Reverse().ToArray()) +
+                   relativeXPath(element);
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                button1_Click(null, null);
+                // Enter key pressed
+            }
+        }
+    }
+}
